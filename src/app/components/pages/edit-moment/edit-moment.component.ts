fe0b050b470f1d24778f231499cb54d6ca10fcc5
Carolina Kaji <carolina.kaji@gmail.com>
2022-10-10 17:33:47 -0300

    Criado Evento quando enviado o formulário, através da edição

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -4,6 +4,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { MomentService } from 'src/app/services/moment.service';
 import { Moment } from 'src/app/Moment';
 
+import { MessagesService } from 'src/app/services/messages.service';
+
 @Component({
   selector: 'app-edit-moment',
   templateUrl: './edit-moment.component.html',
@@ -17,7 +19,8 @@ export class EditMomentComponent implements OnInit {
   constructor(
     private momentService: MomentService,
     private route: ActivatedRoute,
-    private router: Router) {
+    private router: Router,
+    private messageService: MessagesService) {
   }
 
   ngOnInit(): void {
@@ -25,6 +28,23 @@ export class EditMomentComponent implements OnInit {
     this.momentService.getMoment(id).subscribe(item => {
       this.moment = item.data;
     })
+  }
+
+  async editHandler(momentData: Moment) {
+    const id = this.moment.id;
+    const formData = new FormData();
+
+    formData.append('title', momentData.title);
+    formData.append('description', momentData.description);
+
+    if (momentData.image) {
+      formData.append('image', momentData.image);
+    }
+    await this.momentService.updateMoment(id!, formData).subscribe();
+
+    this.messageService.add(`Momento ${id} atualizado com sucesso`);
+
+    this.router.navigate(['/']);
   }
 
 }
