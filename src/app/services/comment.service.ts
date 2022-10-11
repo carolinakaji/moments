aade8188119dbc78d991f3d6bd316ea7a1153182
Carolina Kaji <carolina.kaji@gmail.com>
2022-10-11 09:44:24 -0300

    Ajuste na url para persistir os comentários

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Comment } from '../Comment';
+import { Response } from '../Response';
+import { environment } from 'src/environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
 
-  constructor() { }
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  createComment(data: Comment): Observable<Response<Comment>> {
+    const url = `${this.apiUrl}/${data.momentId}/comments`;
+    return this.http.post<Response<Comment>>(url, data);
+  }
 }
